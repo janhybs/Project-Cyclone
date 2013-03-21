50628b0c034be6571d7d92d3b274b6a5b157c541
Jan Hybš <x3mSpeedy@gmail.com>
2013-03-21 12:55:14 +0100

    Added enemies movement
    Also added enemy sprite

--- public_html/js/modules/enemies.js
+++ public_html/js/modules/enemies.js
@@ -10,14 +10,49 @@ Crafty.c (ENEMY_ABS, {
         this.resistance = toDamage (0);
         this.health = 100;
         this.shield = 0;
-        this.speed = 3;
-        this.path = null;
+        this.speed = 32;
+        this.path = enlargePath (expandPath (splitPath ('0,0 1,0 1,8 8,8 12,8 12,14 15,14 15,0 16,0 16,6')));
+
+        //# current point and next point to go
+        this._cp = null;
+        this._np = null;
+        //# block index (starting at zeroth)
     },
     enterFrame: function () {
-        console.log ('abs');
+
+        //# moving
+        this.x += this.xstep;
+        this.y += this.ystep;
+
+        //# if enemy has reached next point (block)
+        //# generate next coords (xstep, ystep)
+        if (this.isAtNextStop ()) {
+            this.findDirection (++this._bi);
+        }
     },
     start: function () {
+        //# assigning first two path blocks
+        this._bi = 0;
+        this.findDirection (this._bi);
+        this.x = this._cp.x;
+        this.y = this._cp.y;
         this.bind ("EnterFrame", this.enterFrame);
+    },
+    findDirection: function (bi) {
+        if (bi+1 >= this.path.length) {
+            this._bi = 0;
+            this.findDirection (this._bi);
+            return;
+        }
+        this._cp = this.path[bi + 0];
+        this._np = this.path[bi + 1];
+        this.angle = Math.atan2 (this._np.y - this.y, this._np.x - this.x);
+        this.xstep = Math.cos (this.angle) * this.speed;
+        this.ystep = Math.sin (this.angle) * this.speed;
+    },
+    isAtNextStop: function () {
+        var d = distance (this, this._np);
+        return d < 5;
     },
     //#
     getResistance: function () {
@@ -63,7 +98,7 @@ Crafty.c (ENEMY_ABS, {
 
 
 Crafty.c (ENEMY_SIMPLE, {
-    enterFrame: function () {
-        console.log ('enemy');
-    },
+    init: function () {
+        console.log ('-enemy');
+    }
 });
--- public_html/js/scenes/test/enemies.js
+++ public_html/js/scenes/test/enemies.js
@@ -1,5 +1,22 @@
 Crafty.scene (SCENE_ENEMY_TEST, function () {
 
     Crafty.background ('#BBB');
+    
+    var e = Crafty.e ('2D, Canvas, Image, {0}, {1}, enemy'.format (ENEMY_ABS, ENEMY_SIMPLE));
+    e.attr ({w: 33*(.7), h: 37*(.7)});
+    e.start ();
+    
+    var e = Crafty.e ('2D, Canvas, Image, {0}, {1}, enemy'.format (ENEMY_ABS, ENEMY_SIMPLE));
+    e.attr ({w: 33*(.7), h: 37*(.7)});
+    e.setSpeed (3);
+    e.start ();
+    
+    var e = Crafty.e ('2D, Canvas, Image, {0}, {1}, enemy'.format (ENEMY_ABS, ENEMY_SIMPLE));
+    e.attr ({w: 33*(.7), h: 37*(.7)});
+    e.setSpeed (10);
+    e.start ();
+    
+    window.ee = e;
+    console.log (e);
 
 });
