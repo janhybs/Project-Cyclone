a04b00be50459a1bc0f49550f99e1308e05bc2de
Jan Hybš <x3mSpeedy@gmail.com>
2013-03-26 20:46:54 +0100

    Added enemy-shot hit routine
    Preparation for hit routine

--- public_html/js/modules/enemies.js
+++ public_html/js/modules/enemies.js
@@ -45,13 +45,106 @@ Crafty.c (ENEMY_ABS, {
         this.findDirection (this._bi);
         this.requires ('Collision');
         this.bind ("EnterFrame", this.enterFrame);
-        this.onHit (SHOT_P2P, function (array) {
-            for (var i = 0, l = array.length; i < l; i++)
-                array[i].obj.destroy ();
-            this.doSplash ();
-            this.destroy ();
-        });
+        this.onHit (SHOT_ABS, this.processHit);
     },
+    processDeath: function (killerShot) {
+        doSplash (this);
+        this.destroy ();
+    },
+    //#
+    processHit: function (shots) {
+        for (var i = 0, l = shots.length; i < l; i++) {
+            var s = shots[i];
+
+            //# was routine already done?
+            if (!s.isValid)
+                continue;
+            s.invalidate ();
+
+            //# P2P routine
+            if (s.has (SHOT_P2P)) {
+                this.transferDamage (s);
+
+                if (this.isDead ())
+                    return this.processDeath (s);
+                s.doDestroy ();
+            }
+
+
+            //# HOMING routine
+            if (s.has (SHOT_HOMING)) {
+                s.doDestroy ();
+            }
+
+
+            //# SPLASH routine
+            if (s.has (SHOT_SPLASH)) {
+                this.transferDamage (s);
+
+                if (this.isDead ())
+                    return this.processDeath (s);
+            }
+
+
+
+
+            //# LASER routine
+            if (s.has (SHOT_LASER)) {
+                this.transferDamage (s);
+
+                if (this.isDead ())
+                    return this.processDeath (s);
+            }
+
+
+        }
+    },
+    transferDamage: function (shot) {
+        var dmg = shot.getDamage ();
+        var res = this.getResistance ();
+
+        //# shield left? if so, apply electric
+        if (this.shield > 0) {
+            this.shield -= (REZISTANCE - res.electric) * dmg.electric;
+
+            //# shield still left? apply rest with smaller dmg
+            if (this.shield > 0) {
+                this.shield -= (1 / BENEFIT_SHIELD) * (
+                        +(REZISTANCE - res.basic) * dmg.basic
+                        + (REZISTANCE - res.fire) * dmg.fire
+                        + (REZISTANCE - res.poison) * dmg.poison
+                        + (REZISTANCE - res.ice) * dmg.ice);
+
+                //# shield gone? good, -rest will substract health (higher dmg)
+                if (this.shield < 0) {
+                    this.health += this.shield * BENEFIT_SHIELD;
+                    this.shield = 0;
+                }
+
+                //# no shield, -rest will substract health (higher dmg)
+            } else {
+                this.health += this.shield * BENEFIT_SHIELD;
+                this.shield = 0;
+                //# health substraction
+                this.health -= (REZISTANCE - res.basic) * dmg.basic
+                        + (REZISTANCE - res.fire) * dmg.fire
+                        + (REZISTANCE - res.poison) * dmg.poison
+                        + (REZISTANCE - res.ice) * dmg.ice;
+            }
+        } else {
+            //# health substraction
+            this.health -= dmg.electric * (REZISTANCE - res.basic)
+                    + (REZISTANCE - res.basic) * dmg.basic
+                    + (REZISTANCE - res.fire) * dmg.fire
+                    + (REZISTANCE - res.poison) * dmg.poison
+                    + (REZISTANCE - res.ice) * dmg.ice;
+        }
+    },
+    //#
+    isDead: function () {
+        return this.health <= 0;
+    },
+    //#
     findDirection: function (bi) {
         if (bi + 1 >= this.path.length) {
             this.doSplash ();
@@ -70,14 +163,6 @@ Crafty.c (ENEMY_ABS, {
         this.angle = Math.atan2 (this.coords.y - this.y, this.coords.x - this.x);
         this.xstep = Math.cos (this.angle) * this.speed;
         this.ystep = Math.sin (this.angle) * this.speed;
-    },
-    doSplash: function () {
-        var s = shot.get (SHOT_SPLASH);
-        s.setStartPoint (this);
-        s.create (20, 64);
-        s.setTTL (FRAME_RATE);
-        s.setFrameCount (16);
-        s.start ();
     },
     isAtNextStop: function () {
         var d = distance (this, this.coords);
