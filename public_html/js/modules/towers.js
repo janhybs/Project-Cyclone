202d7c3f8bfd6f6754b4176e075dd2dd447275b4
Lukáš Matějů <lukas.mateju@centrum.cz>
2013-03-27 20:37:17 +0100

    tower module upgrade
    additions, reorganization

--- public_html/js/modules/towers.js
+++ public_html/js/modules/towers.js
@@ -1,3 +1,6 @@
+/*** TOWER MODULE ***/
+
+/*** Define as global object ***/
 window.tower = {
     get: function(type) {
         switch (type) {
@@ -7,26 +10,25 @@ window.tower = {
             case TOWER_FLAMETHROWER:
                 return Crafty.e('2D, Canvas, Image, {0}, {1}, {2}, portal'.format(TOWER_ABS, TOWER_P2P, type))
                         .attr({w: W, h: H});
-            case TOWER_LASER:
-                return Crafty.e('2D, Canvas, Image, {0}, {1}, {2}, cat'.format(TOWER_ABS, TOWER_P2P, type))
+            case TOWER_BEAM_LASER:
+                return Crafty.e('2D, Canvas, Image, {0}, {1}, {2}, cat'.format(TOWER_ABS, TOWER_LASER, type))
+                        .attr({w: W, h: H});
+            case TOWER_CHAIN_LASER:
+                return Crafty.e('2D, Canvas, Image, {0}, {1}, {2}, cat'.format(TOWER_ABS, TOWER_LASER, type))
                         .attr({w: W, h: H});
             case TOWER_HOMING_MISSILE:
                 return Crafty.e('2D, Canvas, Image, {0}, {1}, {2}, cat'.format(TOWER_ABS, TOWER_HOMING, type))
                         .attr({w: W, h: H});
-            case TOWER_CHAIN_LASER:
-                return Crafty.e('2D, Canvas, Image, {0}, {1}, {2}, cat'.format(TOWER_ABS, TOWER_P2P, type))
-                        .attr({w: W, h: H});
             case TOWER_ELECTRIC_AURA:
                 return Crafty.e('2D, Canvas, Image, {0}, {1}, {2}, cat'.format(TOWER_ABS, TOWER_SPLASH, type))
-                        .attr({w: W, h: H});
-            case TOWER_CANNON:
-                return Crafty.e('2D, Canvas, Image, {0}, {1}, {2}, {3}, cat'.format(TOWER_ABS, TOWER_P2P, TOWER_SPLASH, type))
                         .attr({w: W, h: H});
         }
     }
 };
 
+/*** ABSTRACT TOWER COMPONENTS ***/
 
+/*** Base Abstract Tower Component ***/
 Crafty.c(TOWER_ABS, {
     abstractCreate: function(startPoint, damage, outputDamage, rate, range, price) {
         this.startPoint = toPoint(startPoint);
@@ -40,7 +42,6 @@ Crafty.c(TOWER_ABS, {
     init: function() {
         this.startPoint = null;
     },
-    //#
     getStartPoint: function() {
         return this.startPoint;
     },
@@ -50,7 +51,6 @@ Crafty.c(TOWER_ABS, {
         this.y = this.startPoint.y;
         return this;
     },
-    //#
     getDamage: function() {
         return this.damage;
     },
@@ -58,7 +58,6 @@ Crafty.c(TOWER_ABS, {
         this.damage = toDamage(damage);
         return this;
     },
-    //#
     getOutputDamage: function() {
         return this.outputDamage;
     },
@@ -66,7 +65,6 @@ Crafty.c(TOWER_ABS, {
         this.outputDamage = toDamage(outputDamage);
         return this;
     },
-    //#
     getRate: function() {
         return this.rate;
     },
@@ -74,7 +72,6 @@ Crafty.c(TOWER_ABS, {
         this.rate = rate;
         return this;
     },
-    //#
     getRange: function() {
         return this.range;
     },
@@ -82,7 +79,6 @@ Crafty.c(TOWER_ABS, {
         this.range = range;
         return this;
     },
-    //#
     getPrice: function() {
         return this.price;
     },
@@ -90,7 +86,6 @@ Crafty.c(TOWER_ABS, {
         this.price = price;
         return this;
     },
-    //#
     getLevel: function() {
         return this.level;
     },
@@ -100,8 +95,29 @@ Crafty.c(TOWER_ABS, {
     }
 });
 
-
+/*** Abstract P2P Tower Component ***/
 Crafty.c(TOWER_P2P, {
+    abstractCreate: function(endPoint, spreading) {
+        this.endPoint = toPoint(endPoint);
+        this.spreding = spreading !== undefined ? spreading : 0;
+    },
+    getEndPoint: function() {
+        return this.endPoint;
+    },
+    setEndPoint: function(value) {
+        this.endPoint = toPoint(value);
+        return this;
+    },
+    getSpreading: function() {
+        return this.spreading;
+    },
+    setSpreading: function(value) {
+        this.spreading = toPoint(value);
+    }   
+});
+
+/*** Abstract Laser Tower Component ***/
+Crafty.c(TOWER_LASER, {
     abstractCreate: function(endPoint) {
         this.endPoint = toPoint(endPoint);
     },
@@ -114,6 +130,7 @@ Crafty.c(TOWER_P2P, {
     }
 });
 
+/*** Abstract Homing Missile Tower Component ***/
 Crafty.c(TOWER_HOMING, {
     abstractCreate: function(endPoint, curving) {
         this.endPoint = toPoint(endPoint);
@@ -135,6 +152,7 @@ Crafty.c(TOWER_HOMING, {
     }
 });
 
+/*** Abstract Splash Tower Component ***/
 Crafty.c(TOWER_SPLASH, {
     abstractCreate: function(growth, radius) {
         this.growth = growth !== undefined ? growth : NaN;
@@ -150,28 +168,36 @@ Crafty.c(TOWER_SPLASH, {
     getRadius: function() {
         return this.radius;
     },
-    setRadois: function(radius) {
+    setRadius: function(radius) {
         this.radius = radius;
         return this;
     }
 });
 
-Crafty.c(TOWER_ELECTRIC_AURA, {
+/*** TOWER COMPONENTS P2P ***/
+
+/*** Machine Gun Tower Component ***/
+Crafty.c(TOWER_MACHINEGUN, {
     create: function() {
-        this.damage = EA_DAMAGE;
-        this.outputDamage = EA_OUTPUT_DAMAGE;
-        this.rate = EA_RATE;
-        this.range = EA_RANGE;
-        this.price = EA_PRICE;
-        this.growth = EA_GROWTH;
-        this.radius = EA_RADIUS;
+        this.damage = MG_DAMAGE;
+        this.outputDamage = MG_OUTPUT_DAMAGE;
+        this.rate = MG_RATE;
+        this.range = MG_RANGE;
+        this.spreading = MG_SPREADING;
+        this.price = MG_PRICE;
     },
+    start: function() {
+        var elems = getEntities(ENEMY_ABS, this, this.range);
+        //projit, najit minimum, predat strele
+    },      
     fire: function() {
-        var s = shot.get(SHOT_SPLASH);
+        var s = shot.get(SHOT_P2P);
         s.setStartPoint([this.startPoint.x, this.startPoint.y]);
+        s.setEndPoint([this.endPoint.x, this.endPoint.y]);
         s.setDamage(this.damage);
+        s.setSpreading(this.spreading);
         s.setTTL(this.range);
-        s.create(this.growth, this.radius);
+        s.create(this.rate);
         s.start();
     },
     upgrade: function() {
@@ -180,6 +206,104 @@ Crafty.c(TOWER_ELECTRIC_AURA, {
     }
 });
 
+/*** Flamethrower Tower Component ***/
+Crafty.c(TOWER_FLAMETHROWER, {
+    create: function() {
+        this.damage = FT_DAMAGE;
+        this.outputDamage = FT_OUTPUT_DAMAGE;
+        this.rate = FT_RATE;
+        this.range = FT_RANGE;
+        this.spreading = FT_SPREADING;
+        this.price = FT_PRICE;
+    },
+    start: function() {
+        var elems = getEntities(ENEMY_ABS, this, this.range);
+        //projit, najit minimum, predat strele
+    },   
+    fire: function() {
+        var s = shot.get(SHOT_P2P);
+        s.setStartPoint([this.startPoint.x, this.startPoint.y]);
+        s.setEndPoint([this.endPoint.x, this.endPoint.y]);
+        s.setDamage(this.damage);
+        s.setSpreading(this.spreading);
+        s.setTTL(this.range);
+        s.create(this.rate);
+        s.start();
+    },
+    upgrade: function() {
+        this.setLevel(this.level + 1);
+        //upgrade sequence - dohodnout
+    }
+});
+
+/*** TOWER COMPONENTS LASER ***/
+
+/*** Beam Laser Tower Component ***/
+Crafty.c(TOWER_BEAM_LASER, {
+    create: function() {
+        this.damage = L_DAMAGE;
+        this.outputDamage = L_OUTPUT_DAMAGE;
+        this.rate = L_RATE;
+        this.range = L_RANGE;
+        this.price = L_PRICE;
+    },
+    start: function() {
+        var elems = getEntities(ENEMY_ABS, this, this.range);
+        //projit, najit minimum, predat strele
+    },  
+    fire: function() {
+        var s = shot.get(SHOT_LASER);
+        s.setStartPoint([this.startPoint.x, this.startPoint.y]);
+        s.setEndPoint([this.endPoint.x, this.endPoint.y]);
+        s.setDamage(this.damage);
+        s.setTTL(this.range);
+        s.create();
+        s.start();
+    },
+    upgrade: function() {
+        this.setLevel(this.level + 1);
+    }
+});
+
+/*** Chain Laser Tower Component ***/
+/*** CHAINING NOT DONE ***/
+Crafty.c(TOWER_CHAIN_LASER, {
+    create: function(endpoint) {
+        this.damage = CHL_DAMAGE;
+        this.outputDamage = CHL_OUTPUT_DAMAGE;
+        this.rate = CHL_RATE;
+        this.range = CHL_RANGE;
+        this.price = CHL_PRICE;
+        this.endPoint2 = toPoint(endpoint);
+    },
+    start: function() {
+        var elems = getEntities(ENEMY_ABS, this, this.range);
+        //projit, najit minimum, predat strele
+    }, 
+    fire: function() {
+        var s = shot.get(SHOT_LASER);
+        s.setStartPoint([this.startPoint.x, this.startPoint.y]);
+        s.setEndPoint([this.endPoint.x, this.endPoint.y]);
+        s.setDamage(this.damage);
+        s.setTTL(this.range);
+        s.create();
+        s.start();
+        var s2 = shot.get(SHOT_LASER);
+        s2.setStartPoint([this.endPoint.x, this.endPoint.y]);
+        s2.setEndPoint([this.endPoint2.x, this.endPoint2.y]);
+        s2.setDamage(this.damage);
+        s2.setTTL(this.range);
+        s2.create();
+        s2.start();
+    },
+    upgrade: function() {
+        this.setLevel(this.level + 1);
+    }
+});
+
+/*** TOWER COMPONENTS HOMING ***/
+
+/*** Homing Missile Tower Component ***/
 Crafty.c(TOWER_HOMING_MISSILE, {
     create: function() {
         this.damage = HM_DAMAGE;
@@ -189,6 +313,10 @@ Crafty.c(TOWER_HOMING_MISSILE, {
         this.price = HM_PRICE;
         this.curving = HM_CURVING;
     },
+    start: function() {
+        var elems = getEntities(ENEMY_ABS, this, this.range);
+        //projit, najit minimum, predat strele
+    }, 
     fire: function() {
         var s = shot.get(SHOT_HOMING);
         s.setStartPoint([this.startPoint.x, this.startPoint.y]);
@@ -204,118 +332,32 @@ Crafty.c(TOWER_HOMING_MISSILE, {
     }
 });
 
-Crafty.c(TOWER_MACHINEGUN, {
+/*** TOWER COMPONENTS SPLASH ***/
+
+/*** Electric Aura Tower Component ***/
+Crafty.c(TOWER_ELECTRIC_AURA, {
     create: function() {
-        this.damage = MG_DAMAGE;
-        this.outputDamage = MG_OUTPUT_DAMAGE;
-        this.rate = MG_RATE;
-        this.range = MG_RANGE;
-        this.price = MG_PRICE;
+        this.damage = EA_DAMAGE;
+        this.outputDamage = EA_OUTPUT_DAMAGE;
+        this.rate = EA_RATE;
+        this.range = EA_RANGE;
+        this.price = EA_PRICE;
+        this.growth = EA_GROWTH;
+        this.radius = EA_RADIUS;
     },
+    start: function() {
+        fire();
+    }, 
     fire: function() {
-        var s = shot.get(SHOT_P2P);
+        var s = shot.get(SHOT_SPLASH);
         s.setStartPoint([this.startPoint.x, this.startPoint.y]);
-        s.setEndPoint([this.endPoint.x, this.endPoint.y]);
         s.setDamage(this.damage);
         s.setTTL(this.range);
-        s.create(this.rate);
+        s.create(this.growth, this.radius);
         s.start();
     },
     upgrade: function() {
         this.setLevel(this.level + 1);
         //upgrade sequence - dohodnout
-    }
-});
-
-Crafty.c(TOWER_FLAMETHROWER, {
-    create: function() {
-        this.damage = FT_DAMAGE;
-        this.outputDamage = FT_OUTPUT_DAMAGE;
-        this.rate = FT_RATE;
-        this.range = FT_RANGE;
-        this.price = FT_PRICE;
-    },
-    fire: function() {
-        var s = shot.get(SHOT_P2P);
-        s.setStartPoint([this.startPoint.x, this.startPoint.y]);
-        s.setEndPoint([this.endPoint.x, this.endPoint.y]);
-        s.setDamage(this.damage);
-        s.setTTL(this.range);
-        s.create(this.rate);
-        s.start();
-// IN PROGRESS
-//        var s2 = shot.get(SHOT_P2P);
-//        s2.setStartPoint([this.startPoint.x, this.startPoint.y]);
-//        s2.setEndPoint([this.endPoint.x, this.endPoint.y]);
-//        s2.setDamage(this.damage);
-//        s2.setTTL(this.range);
-//        s2.create(this.rate);
-//        s2.start();
-//
-//        var s3 = shot.get(SHOT_P2P);
-//        s3.setStartPoint([this.startPoint.x, this.startPoint.y]);
-//        s3.setEndPoint([this.endPoint.x, this.endPoint.y]);
-//        s3.setDamage(this.damage);
-//        s3.setTTL(this.range);
-//        s3.create(this.rate);
-//        s3.start();
-    },
-    upgrade: function() {
-        this.setLevel(this.level + 1);
-        //upgrade sequence - dohodnout
-    }
-});
-
-Crafty.c(TOWER_LASER, {
-    create: function() {
-        this.damage = L_DAMAGE;
-        this.outputDamage = L_OUTPUT_DAMAGE;
-        this.rate = L_RATE;
-        this.range = L_RANGE;
-        this.price = L_PRICE;
-    },
-    fire: function() {
-        var s = shot.get(SHOT_LASER);
-        s.setStartPoint([this.startPoint.x, this.startPoint.y]);
-        s.setEndPoint([this.endPoint.x, this.endPoint.y]);
-        s.setDamage(this.damage);
-        s.setTTL(this.range);
-        s.create();
-        s.start();
-    },
-    upgrade: function() {
-        this.setLevel(this.level + 1);
-    }
-});
-
-//generalize input
-Crafty.c(TOWER_CHAIN_LASER, {
-    create: function(endpoint) {
-        this.damage = CHL_DAMAGE;
-        this.outputDamage = CHL_OUTPUT_DAMAGE;
-        this.rate = CHL_RATE;
-        this.range = CHL_RANGE;
-        this.price = CHL_PRICE;
-        this.endPoint2 = toPoint(endpoint);
-    },
-    fire: function() {
-        var s = shot.get(SHOT_LASER);
-        s.setStartPoint([this.startPoint.x, this.startPoint.y]);
-        s.setEndPoint([this.endPoint.x, this.endPoint.y]);
-        s.setDamage(this.damage);
-        s.setTTL(this.range);
-        s.create();
-        s.start();
-
-        var s2 = shot.get(SHOT_LASER);
-        s2.setStartPoint([this.endPoint.x, this.endPoint.y]);
-        s2.setEndPoint([this.endPoint2.x, this.endPoint2.y]);
-        s2.setDamage(this.damage);
-        s2.setTTL(this.range);
-        s2.create();
-        s2.start();
-    },
-    upgrade: function() {
-        this.setLevel(this.level + 1);
     }
 });
