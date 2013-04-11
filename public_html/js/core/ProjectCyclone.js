9e0cb1a5dcff531a2a6e78308714ebf5f2dd084d
Jan Hybš <x3mSpeedy@gmail.com>
2013-04-11 21:08:42 +0200

    Added webview div to display non-canvas stuff
    Added images of player types, creates basic css layout for new game

--- public_html/js/core/ProjectCyclone.js
+++ public_html/js/core/ProjectCyclone.js
@@ -1,8 +1,13 @@
 ProjectCyclone = {
     //start crafty :) 
-    start: function() {
-        Crafty.init(SCREEN_WIDTH, SCREEN_HEIGHT);
-        Crafty.canvas.init();
-        Crafty.scene(SCENE_LOADING);
+    start: function () {
+        Crafty.init (SCREEN_WIDTH, SCREEN_HEIGHT);
+        Crafty.canvas.init ();
+        Crafty.scene (SCENE_LOADING);
+
+        $ ('#cr-stage').before ($ ('<div id="webview"></div>')[0]);
+        $ ('#webview').load ('pages.html #scene-new-game');
+        
+        $ ('#cr-stage').hide ();
     }
 };
