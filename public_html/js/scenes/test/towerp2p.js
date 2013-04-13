55d5f85930af888d0cd5596a59dbb6770cebe93a
Lukáš Matějů <lukas.mateju@centrum.cz>
2013-04-13 21:56:34 +0200

    testing scene

--- public_html/js/scenes/test/towerp2p.js
+++ public_html/js/scenes/test/towerp2p.js
@@ -1,43 +1,47 @@
 Crafty.scene(SCENE_P2P_TOWER_TEST, function() {
 
     Crafty.background('#333');
+    Crafty.e ("2D, Canvas, Image").attr ({w: SCREEN_WIDTH, h: SCREEN_HEIGHT, alpha: 0.25}).image ("images/sq.jpg", "repeat");
 
-    var p0 = {x: 2, y: 2};
-    var p1 = {x: 20, y: 15};
-    var p2 = {x: 10, y: 10};
-    var p3 = {x: 20, y: 2};
+    var path1 = enlargePath (expandPath (splitPath ('0,0 1,0 1,8 8,8 12,8 12,14 15,14 15,0 19,0 19,6')));
+    var p0 = {x: 4, y: 2};
+    var p1 = {x: 4, y: 10};
+    var p2 = {x: 5, y: 6};
+
+    var timer = Crafty.e ('Framer');
+
+    timer.repeat (function () {
+        var e = enemy.create ({
+            path: path1,
+            shield: ENEMY_SHIELD.no,
+            size: ENEMY_SIZE.small,
+            speed: ENEMY_SPEED.slow,
+            resistance: arrayMerge (arrayMult (ENEMY_TYPE.normal, 0.8), arrayMult (ENEMY_TYPE.fire, 0.2))
+        });
+        e.start ();
+        e.requires ('HealthBar');
+    }, FRAME_RATE);
 
     var t = tower.get(TOWER_MACHINEGUN);
     t.setStartPoint([p0.x * W, p0.y * H]);
     t.create();
     t.upgrade();
-    setInterval(function() {
-        t.setEndPoint([Crafty.math.randomInt(0, SCREEN_WIDTH / W) * W, Crafty.math.randomInt(0, SCREEN_HEIGHT / H) * H]);
-        t.fire();
-    }, 100);
+    timer.repeat(function() {
+        t.start();
+    }, FRAME_RATE / 5);
 
-    var t2 = tower.get(TOWER_CHAIN_LASER);
-    t2.setStartPoint([p3.x * W, p3.y * H]);
-    t2.setEndPoint([Crafty.math.randomInt(0, SCREEN_WIDTH / W) * W, Crafty.math.randomInt(0, SCREEN_HEIGHT / H) * H]);
-    t2.create([p0.x * W, p0.y * H]);
-    t2.fire();
+    var t2 = tower.get(TOWER_CANNON);
+    t2.setStartPoint([p1.x * W, p1.y * H]);
+    t2.create();
+    timer.repeat(function() {
+        t2.start();
+    }, FRAME_RATE / 1);
     
     var t3 = tower.get(TOWER_HOMING_MISSILE);
-    t3.setStartPoint([p1.x * W, p1.y * H]);
+    t3.setStartPoint([p2.x * W, p2.y * H]);
     t3.create();
-    t3.upgrade();
-    setInterval(function() {
-        t3.setEndPoint([Crafty.math.randomInt(0, SCREEN_WIDTH / W) * W, Crafty.math.randomInt(0, SCREEN_HEIGHT / H) * H]);
-        t3.fire();
-    }, 100);
+    timer.repeat(function() {
+        t3.start();
+    }, FRAME_RATE / 2);
 
-    var t4 = tower.get(TOWER_ELECTRIC_AURA);
-    t4.setStartPoint([p1.x * W, p1.y * H]);
-    t4.create();
-    setInterval(function() {
-        t4.setStartPoint([Crafty.math.randomInt(0, SCREEN_WIDTH / W) * W, Crafty.math.randomInt(0, SCREEN_HEIGHT / H) * H]);
-        t4.fire();
-    }, 100);
-
-    
 });
