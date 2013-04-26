815a73609c7763970157815ea752590a4e92d9b1
frkovnik <pav3l.novak@gmail.com>
2013-04-26 22:42:29 +0200

    new player utils methods

--- public_html/js/core/playerUtils.js
+++ public_html/js/core/playerUtils.js
@@ -7,28 +7,55 @@ PlayerUtils.newGameInitialize = function(nameGunPlayer, nameLaserPlayer) {
     $.jStorage.set(ACTIVE_PLAYER, GUN_PLAYER);
     $.jStorage.set(GUN_PLAYER_EXPS, 0);
     $.jStorage.set(GUN_PLAYER_LEVEL, 1);
-    $.jStorage.set(GUN_PLAYER_AVAILABLE_POINTS, 0);
-    $.jStorage.set(GUN_PLAYER_USED_POINTS, 0);
+    $.jStorage.set(GUN_PLAYER_DAMAGE, 1);
+    $.jStorage.set(GUN_PLAYER_RANGE, 1);
     $.jStorage.set(LASER_PLAYER_EXPS, 0);
     $.jStorage.set(LASER_PLAYER_LEVEL, 1);
-    $.jStorage.set(LASER_PLAYER_AVAILABLE_POINTS, 0);
-    $.jStorage.set(LASER_PLAYER_USED_POINTS, 0);
+    $.jStorage.set(LASER_PLAYER_DAMAGE, 1);
+    $.jStorage.set(LASER_PLAYER_RANGE, 1);
 };
 
 //method returns actualLevel of actual player
 PlayerUtils.getActualLevel = function() {
-    if(localStorage[ACTIVE_PLAYER] === GUN_PLAYER) {
-        return localStorage[GUN_PLAYER_LEVEL];            
+    if($.jStorage.get(ACTIVE_PLAYER) === GUN_PLAYER) {
+        return $.jStorage.get(GUN_PLAYER_LEVEL);            
     } else {
-        return localStorage[LASER_PLAYER_LEVEL];
+        return $.jStorage.get(LASER_PLAYER_LEVEL);
     }
 };
 
 //method returns actualLevel by player type
 PlayerUtils.getActualLevelByPlayerType = function(type) {
     if(type === GUN_PLAYER) {
-        return localStorage[GUN_PLAYER_LEVEL];            
+        return $.jStorage.get(GUN_PLAYER_LEVEL);            
     } else {
-        return localStorage[LASER_PLAYER_LEVEL];
+        return $.jStorage.get(LASER_PLAYER_LEVEL);
+    }
+};
+
+//method returns name of actual player
+PlayerUtils.getActualName = function() {
+    if($.jStorage.get(ACTIVE_PLAYER) === GUN_PLAYER) {
+        return $.jStorage.get(GUN_PLAYER_NAME);            
+    } else {
+        return $.jStorage.get(LASER_PLAYER_NAME);
+    }
+};
+
+//method adds one damage point of actual player
+PlayerUtils.addDamagePoint = function() {
+        if($.jStorage.get(ACTIVE_PLAYER) === GUN_PLAYER) {
+            $.jStorage.set(GUN_PLAYER_DAMAGE, $.jStorage.get(GUN_PLAYER_DAMAGE) + 1);            
+    } else {
+            $.jStorage.set(LASER_PLAYER_DAMAGE, $.jStorage.get(LASER_PLAYER_DAMAGE) + 1);
+    }
+};
+
+//method adds one range point of actual player
+PlayerUtils.addRangePoint = function() {
+        if($.jStorage.get(ACTIVE_PLAYER) === GUN_PLAYER) {
+            $.jStorage.set(GUN_PLAYER_RANGE, $.jStorage.get(GUN_PLAYER_RANGE) + 1);            
+    } else {
+            $.jStorage.set(LASER_PLAYER_RANGE, $.jStorage.get(LASER_PLAYER_RANGE) + 1);
     }
 };
