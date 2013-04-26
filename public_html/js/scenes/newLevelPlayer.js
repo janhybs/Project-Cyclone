78c9a25115a9511cd7a7714f81b39dfce8f24db0
frkovnik <pav3l.novak@gmail.com>
2013-04-26 22:59:16 +0200

    complete player upg

--- public_html/js/scenes/newLevelPlayer.js
+++ public_html/js/scenes/newLevelPlayer.js
@@ -4,6 +4,10 @@ Crafty.scene (SCENE_LEVEL_UP,
             activateWebview ();
             loadPage ('webview', 'scene-level-up', function () {
                 $ ('#scene-new-game-next');
+            $("#actPlayer").html("Your player {0} has".format(PlayerUtils.getActualName()));
+            $("#actLevel").html("LEVEL {0}".format(PlayerUtils.getActualLevel()));
+            $('#damage').html(PlayerUtils.getDamagePoints())
+            $('#range').html(PlayerUtils.getRangePoints())
             });
         },
         function () {
