57a5bbedbde34aaaab3a4f561e04c91e1f731c37
frkovnik <pav3l.novak@gmail.com>
2013-04-26 19:28:53 +0200

    new player utils methods

--- public_html/js/core/playerUtils.js
+++ public_html/js/core/playerUtils.js
@@ -1,7 +1,7 @@
 window.PlayerUtils = {};
 
+//new game init
 PlayerUtils.newGameInitialize = function(nameGunPlayer, nameLaserPlayer) {
-    localStorage[GUN_PLAYER_NAME] = nameGunPlayer;
     localStorage[GUN_PLAYER_NAME] = nameGunPlayer;
     localStorage[LASER_PLAYER_NAME] = nameLaserPlayer;
     localStorage[ACTIVE_PLAYER] = GUN_PLAYER;
@@ -13,4 +13,22 @@ PlayerUtils.newGameInitialize = function(nameGunPlayer, nameLaserPlayer) {
     localStorage[LASER_PLAYER_LEVEL] = 1;
     localStorage[LASER_PLAYER_AVAILABLE_POINTS] = 0;
     localStorage[LASER_PLAYER_USED_POINTS] = 0;
+};
+
+//method returns actualLevel of actual player
+PlayerUtils.getActualLevel = function() {
+    if(localStorage[ACTIVE_PLAYER] === GUN_PLAYER) {
+        return localStorage[GUN_PLAYER_LEVEL];            
+    } else {
+        return localStorage[LASER_PLAYER_LEVEL];
+    }
+};
+
+//method returns actualLevel by player type
+PlayerUtils.getActualLevelByPlayerType = function(type) {
+    if(type === GUN_PLAYER) {
+        return localStorage[GUN_PLAYER_LEVEL];            
+    } else {
+        return localStorage[LASER_PLAYER_LEVEL];
+    }
 };
