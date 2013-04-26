83f800e9bcdfafa8dc54fbf70fc2c5349ab4326d
frkovnik <pav3l.novak@gmail.com>
2013-04-26 20:44:39 +0200

    change storing to jstorage

--- public_html/js/core/playerUtils.js
+++ public_html/js/core/playerUtils.js
@@ -2,17 +2,17 @@ window.PlayerUtils = {};
 
 //new game init
 PlayerUtils.newGameInitialize = function(nameGunPlayer, nameLaserPlayer) {
-    localStorage[GUN_PLAYER_NAME] = nameGunPlayer;
-    localStorage[LASER_PLAYER_NAME] = nameLaserPlayer;
-    localStorage[ACTIVE_PLAYER] = GUN_PLAYER;
-    localStorage[GUN_PLAYER_EXPS] = 0;
-    localStorage[GUN_PLAYER_LEVEL] = 1;
-    localStorage[GUN_PLAYER_AVAILABLE_POINTS] = 0;
-    localStorage[GUN_PLAYER_USED_POINTS] = 0;
-    localStorage[LASER_PLAYER_EXPS] = 0;
-    localStorage[LASER_PLAYER_LEVEL] = 1;
-    localStorage[LASER_PLAYER_AVAILABLE_POINTS] = 0;
-    localStorage[LASER_PLAYER_USED_POINTS] = 0;
+    $.jStorage.set(GUN_PLAYER_NAME, nameGunPlayer);
+    $.jStorage.set(LASER_PLAYER_NAME, nameLaserPlayer);
+    $.jStorage.set(ACTIVE_PLAYER, GUN_PLAYER);
+    $.jStorage.set(GUN_PLAYER_EXPS, 0);
+    $.jStorage.set(GUN_PLAYER_LEVEL, 1);
+    $.jStorage.set(GUN_PLAYER_AVAILABLE_POINTS, 0);
+    $.jStorage.set(GUN_PLAYER_USED_POINTS, 0);
+    $.jStorage.set(LASER_PLAYER_EXPS, 0);
+    $.jStorage.set(LASER_PLAYER_LEVEL, 1);
+    $.jStorage.set(LASER_PLAYER_AVAILABLE_POINTS, 0);
+    $.jStorage.set(LASER_PLAYER_USED_POINTS, 0);
 };
 
 //method returns actualLevel of actual player
