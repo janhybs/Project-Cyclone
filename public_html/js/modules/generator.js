ac92b38499b28532cba7caf9878440ae696ca686
Jan Hybš <x3mSpeedy@gmail.com>
2013-05-05 21:48:03 +0200

    Enemy generator improved

--- public_html/js/modules/generator.js
+++ public_html/js/modules/generator.js
@@ -6,45 +6,66 @@ window.generator = {
     //-------------------------
     start: function (data, levelPaths) {
         this.timer = Crafty.e ('Framer');
+        this.watchDog = Crafty.e ('Framer');
+        this.watchDogID = this.watchDog.repeat (function () {
+            if (Crafty (ENEMY_ABS).length === 0 && this.everythingReleased) {
+                this.watchDog.clearTimer (this.watchDogID);
+                Crafty.trigger (GAME_END, $.livesTotal - $.livesLeft);
+            }
+        }, 25, this);
         this.xmlData = data;
         this.i = 0;
         this.paths = [];
-        for (var i = 0; i < levelPaths.length; i++) {
-             this.paths.push (enlargePath (levelPaths[i]));
-        }
-        this.nextWave (0);
-        
+        this.currentWave = -1;
+        this.currentPart = -1;
+        this.autoPlay = true;
+        this.totalWaves = this.xmlData.waves.wave.length;
+        this.everythingReleased = false;
+
+        for (var i = 0; i < levelPaths.length; i++)
+            this.paths.push (enlargePath (levelPaths[i]));
     },
-    nextWave: function (i) {
-//    for (var w in xmlData.waves.wave) {
-//    for (var j in xmlData.waves.wave[i][j]) {
-        timer.repeat (function () {
-            timer.repeat (function () {
+    nextWave: function () {
+        //# end round
+        if (!this.incWave ())
+            return;
+        this.nextPart ();
+    },
+    nextPart: function () {
+        //# end wave
+        if (!this.incPart ()) {
+            if (this.currentWave + 1 === this.totalWaves) {
+                this.everythingReleased = true;
+            }
+
+            if (this.autoPlay)
+                this.nextWave ();
+            return;
+        }
+
+        var o = this.xmlData.waves.wave[this.currentWave].item[this.currentPart];
+        this.timer.repeat (function () {
+            this.timer.repeat (function () {
+                var generator = enemy.parse (JSON.parse (o.generator));
                 for (var j = 0; j < this.paths.length; j++) {
-                    enemy.create ({
-                        path: this.paths[j], speed: ENEMY_SPEED.lighbolt, shield: ENEMY_SHIELD.boss, wobble: ENEMY_WOBBLE.no
-                    }).requires ('HealthBar').start ();
+                    enemy.create (generator).requires ('HealthBar')
+                            .setPath (this.paths[j])
+                            .start ();
                 }
-            },
-                    this.xmlData.waves.wave[i].item[0].frame, this,
-                    this.xmlData.waves.wave[i].item[0].count);
-        },
-                this.xmlData.waves.wave[i].item[0].delay, this,
-                this.xmlData.waves.wave[i].item[0].repeat);
+            }, o.frame, this, o.count);
+        }, o.delay, this, o.repeat);
+
+        var partTime = (Number (o.count) * Number (o.frame) + Number (o.delay)) * Number (o.repeat);
+
+        this.timer.delay (function () {
+            this.nextPart ();
+        }, partTime, this);
+    },
+    incWave: function () {
+        this.currentPart = -1;
+        return ++this.currentWave < this.xmlData.waves.wave.length;
+    },
+    incPart: function () {
+        return ++this.currentPart < this.xmlData.waves.wave[this.currentWave].item.length;
     }
-
-
 };
-
-
-
-
-
-//nextWave: function (i) {
-////    for (var w in xmlData.waves.wave) {
-////    for (var j in xmlData.waves.wave[i][j]) {
-//console.log (xmlData.waves.wave[i][0]);
-//        timer.repeat (function () {
-//console.log ('ted');
-//}, xmlData.waves.wave[i][0].repeat);
-//}
