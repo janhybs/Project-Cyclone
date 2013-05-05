7678cf5a760235e8cb09824a338e50812c3ab292
frkovnik <pav3l.novak@gmail.com>
2013-05-06 00:15:52 +0200

    add money fix

--- public_html/js/scenes/gameComplete.js
+++ public_html/js/scenes/gameComplete.js
@@ -3,8 +3,8 @@ Crafty.scene (SCENE_GAME_SUCCESS,
 
             activateWebview ();
             loadPage ('webview', 'scene-game-complete', function () {
-                var maxMoney = MONEY_BY_LEVEL[$.actualLevel];
-                var earnedMoney = maxMoney - (maxMoney * Math.round(($.livesLeft - $.livesTotal)/$.livesTotal));
+                var maxMoney = MONEY_BY_LEVEL[$.actualLevel-1];
+                var earnedMoney = maxMoney - (maxMoney * Math.round((($.livesTotal- $.livesLeft)/$.livesTotal)*100)/100);
                 $("#recMoney").html(earnedMoney);
                 PlayerUtils.addPlayerMoney(earnedMoney);
             });
