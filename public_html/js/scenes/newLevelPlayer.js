371ab06089368651ca415145be9bde27923ff89b
Lukáš Matějů <lukas.mateju@centrum.cz>
2013-05-18 16:16:18 +0200

    scenes unified, buttons bugfix

--- public_html/js/scenes/newLevelPlayer.js
+++ public_html/js/scenes/newLevelPlayer.js
@@ -3,7 +3,7 @@ Crafty.scene (SCENE_LEVEL_UP,
             PlayerUtils.increaseLevel();
             activateWebview ();
             loadPage ('webview', 'scene-level-up', function () {
-            $("#actPlayer").html("Your player {0} has".format(PlayerUtils.getActualName()));
+            $("#actPlayer").html("Your player {0} leveled up.".format(PlayerUtils.getActualName()));
             $("#actLevel").html("LEVEL {0}".format(PlayerUtils.getActualLevel()));
             $('#damage').html(PlayerUtils.getDamagePoints());
             $('#range').html(PlayerUtils.getRangePoints());
